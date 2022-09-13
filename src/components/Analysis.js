02717dd4f64134e6c31fc96d6aeed8f80562a620
Md. Rakib Islam <rakibislam.nitex@gmail.com>
2022-09-13 18:11:13 +0600

    result jsx done

---
+++ src/components/Analysis.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Analysis = () => {
+    return (
+        <div className="analysis">
+            <h1>Question Analysis</h1>
+            <h4>You answerd 5 out of 10 questions correctly</h4>
+        </div>
+    );
+};
+
+export default Analysis;
---
+++ src/components/pages/Result.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Analysis from "../Analysis";
+import Question from "../Question";
+import Summary from "../Summary";
+
+const Result = () => {
+    return (
+        <>
+            <Summary />
+            <Analysis />
+            <Question />
+        </>
+    );
+};
+
+export default Result;
